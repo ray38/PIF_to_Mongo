1e7d79d91c008ea88923972f3b8728f6cc1ed684
Ray Lei <xlei38@gatech.edu>
2017-05-08 15:31:56 -0400

    initial commit

--- spider.py
+++ spider.py
@@ -66,6 +66,6 @@ def DFT_query(database = 'PIFs', collection = 'DFT', query = {}, username = None
 if __name__ == "__main__":
     username, password = user_authentication()
     DFT_spider(database = 'PIFs', collection = 'DFT', username = username, password = password)
-    DFT_query(database = 'PIFs', collection = 'DFT', query = {}, username = username, password = password)
+    DFT_query(database = 'PIFs', collection = 'DFT', query = {'path':'/gpfs/pace1/project/chbe-medford/medford-share/users/bcomer3/espresso_rutile/espresso_Rutile/2_layer_runs/N/test/esp.log'}, username = username, password = password)
 #for post in collection.find():
 #    pprint.pprint(post)
